e517e18b470bae1d28d6cb92f8df4693bdf41b84
Giuseppe Lisi <giuseppe.lisi.main@gmail.com>
2026-02-19 17:11:54 +0100

    implementato il router all'interno di app.js

--- index.js
+++ index.js
@@ -1,35 +1,15 @@
 const express = require("express");
 const app = express();
 const port = 3000;
-const posts = require("./posts.js");
+const postsRouter = require("./router/posts.js")
 
 app.get("/", (req, res) => {
     res.send("<h1>Benvenuto nel Server del Blog</h1>");
 });
 
-app.get("/posts", (req, res) => {
-    res.send("Lista dei Post")
-})
-
-app.get("/posts/:id", (req, res) => {
-    res.send(`Restituisco post con id: ${req.params.id}`);
-})
-
-app.post("/posts", (req, res) => {
-    res.send("Aggiungo post");
-})
-
-app.put("/posts/:id", (req, res) => {
-    res.send(`Modifico interamento l'elemento con id: ${req.params.id}`);
-})
-
-app.patch("/posts/:id", (req, res) => {
-    res.send(`Aggiorno l'elemento con id: ${req.params.id}`);
-})
-
-app.delete("/posts/:id", (req, res) => {
-    res.send(`Cancello l'elemento con id: ${req.params.id}`);
-})
+// screma l'index.js lasciando la gestione delle richieste CRUD 
+// al router di express contenuto in un file esterno posts.js
+app.use("/posts", postsRouter);
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
