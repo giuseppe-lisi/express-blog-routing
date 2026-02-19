cb5164a855b6898c1452ef055793aeac4635dd82
Giuseppe Lisi <giuseppe.lisi.main@gmail.com>
2026-02-19 17:12:08 +0100

    posts.js gestisce ora le richieste fatte al server

---
+++ router/posts.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const router = express.Router();
+
+router.get("/ ", (req, res) => {
+    res.send("Lista dei Post")
+})
+
+router.get("/:id", (req, res) => {
+    res.send(`Restituisco post con id: ${req.params.id}`);
+})
+
+router.post("/", (req, res) => {
+    res.send("Aggiungo post");
+})
+
+router.put("/:id", (req, res) => {
+    res.send(`Modifico interamento l'elemento con id: ${req.params.id}`);
+})
+
+router.patch("/:id", (req, res) => {
+    res.send(`Aggiorno l'elemento con id: ${req.params.id}`);
+})
+
+router.delete("/:id", (req, res) => {
+    res.send(`Cancello l'elemento con id: ${req.params.id}`);
+})
+
+module.exports = router;
