0fb9cf04929c518cd22f0746f9bf5f9bd84aad84
Giuseppe Lisi <giuseppe.lisi.main@gmail.com>
2026-02-19 17:03:32 +0100

    scritte le rotte per tutte le operazioni CRUD

--- index.js
+++ index.js
@@ -1,10 +1,35 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+const posts = require("./posts.js");
 
 app.get("/", (req, res) => {
     res.send("<h1>Benvenuto nel Server del Blog</h1>");
 });
+
+app.get("/posts", (req, res) => {
+    res.send("Lista dei Post")
+})
+
+app.get("/posts/:id", (req, res) => {
+    console.log(`Restituisco post con id: ${req.params.id}`);
+})
+
+app.post("/posts", (req, res) => {
+    console.log("Aggiungo post");
+})
+
+app.put("/posts/:id", (req, res) => {
+    console.log(`Modifico interamento l'elemento con id: ${req.params.id}`);
+})
+
+app.patch("/posts/:id", (req, res) => {
+    console.log(`Aggiorno l'elemento con id: ${req.params.id}`);
+})
+
+app.delete("/posts/:id", (req, res) => {
+    console.log(`Cancello l'elemento con id: ${req.params.id}`);
+})
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
