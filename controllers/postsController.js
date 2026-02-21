c458af87879cd0482fd28d136d3ea3ad0a7c5e0c
Giuseppe Lisi <giuseppe.lisi.main@gmail.com>
2026-02-21 15:42:54 +0100

    cambiato formato risposta errore show e destroy in json

--- controllers/postsController.js
+++ controllers/postsController.js
@@ -7,7 +7,7 @@ function index(req, res) {
 function show(req, res) {
     const post = posts.find((post) => post.id == req.params.id);
     if (isNaN(req.params.id) || !post) {
-        res.status(404).send(
+        res.status(404).json(
             `Id "${req.params.id}" is not valid, no such post exists`,
         );
     } else {
@@ -32,7 +32,7 @@ function destroy(req, res) {
     // se viene passato un id che non è un num o non esiste un post con
     // l'id specificato restituisce un errore
     if (isNaN(req.params.id) || !postToDelete) {
-        res.status(404).send(
+        res.status(404).json(
             `Error: post doesnt exist or bad id "${req.params.id}"`,
         );
         // se il post esiste lo elimina dall'array dei post e restituire status ok
