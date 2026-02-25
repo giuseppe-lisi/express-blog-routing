bb58def6d10284daf7fbedf2a341ee7864eb57fd
Giuseppe Lisi <giuseppe.lisi.main@gmail.com>
2026-02-25 11:41:39 +0100

    aggiunto parser json per i body delle request

--- index.js
+++ index.js
@@ -2,13 +2,16 @@ const express = require("express");
 const app = express();
 const port = 3000;
 const postsRouter = require("./router/postsRouter.js")
+const bodyParser = require('body-parser')
 
 app.get("/", (req, res) => {
     res.send("<h1>Benvenuto nel Server del Blog</h1>");
 });
 
 // amplio le funzionalità di express usando il router che gestisce le rotte
+// e il body parser per poter leggere i post in arrivo
 app.use("/posts", postsRouter);
+app.use(bodyParser.json());
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
